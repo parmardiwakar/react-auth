1b4564831ed02e276b0aa2276161aa717858704b
Diwakar Parmar <diwakarparmar@gmail.com>
2023-03-04 01:09:06 -0500

    added route protection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ import AuthenticationPage, {
   action as authAction,
 } from "./pages/Authentication";
 import { action as logoutAction } from "./pages/Logout";
-import { loader as tokenLoader } from "./utils/auth";
+import { tokenLoader, checkAuthLoader } from "./utils/auth";
 
 const router = createBrowserRouter([
   {
@@ -51,6 +51,7 @@ const router = createBrowserRouter([
                 path: "edit",
                 element: <EditEventPage />,
                 action: manipulateEventAction,
+                loader: checkAuthLoader,
               },
             ],
           },
@@ -58,6 +59,7 @@ const router = createBrowserRouter([
             path: "new",
             element: <NewEventPage />,
             action: manipulateEventAction,
+            loader: checkAuthLoader,
           },
         ],
       },
