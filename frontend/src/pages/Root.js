e568a431ced37da004003473abb27ba26061141f
Diwakar Parmar <diwakarparmar@gmail.com>
2023-03-04 01:27:04 -0500

    automatic logout after specified time

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,15 +1,34 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { useEffect } from "react";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { getTokenDuration } from "../utils/auth";
 
 function RootLayout() {
-  // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "logout", method: "post" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    setTimeout(() => {
+      submit(null, { action: "logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
 
   return (
     <>
       <MainNavigation />
       <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,7 +1,27 @@
 import { redirect } from "react-router-dom";
 
+export function getTokenDuration() {
+  const storedExpirationDate = localStorage.getItem("expiration");
+  const expirationDate = new Date(storedExpirationDate);
+  const now = new Date();
+  const duration = expirationDate.getTime() - now.getTime();
+
+  return duration;
+}
+
 export function getAuthToken() {
   const token = localStorage.getItem("token");
+
+  if(!token) {
+    return null;
+  }
+
+  const tokenDuration = getTokenDuration();
+
+  if(tokenDuration < 0) {
+    return "EXPIRED";
+  }
+
   return token;
 }
 
@@ -12,7 +32,7 @@ export function tokenLoader() {
 export function checkAuthLoader() {
   const token = getAuthToken();
 
-  if(!token) {
+  if (!token) {
     return redirect("/auth");
   }
 
